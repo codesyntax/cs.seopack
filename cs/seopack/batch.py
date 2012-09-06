3607720d969c519d20c303624d41bc7be04021f4
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-09-06 14:35:47 +0200

    check for none

--- cs/seopack/batch.py
+++ cs/seopack/batch.py
@@ -4,9 +4,13 @@ class BatchNoIndex(ViewletBase):
 
     def show(self):
         start = self.request.get('b_start', None)
+        if start is None:
+            return False
+            
         try:
             b_start = int(start)
             return b_start != 0
         except ValueError:
             return False
+
         
