844d39e8bd78b8afb131aaba6ffc5528c8195724
Mikel Larreategi <mlarreategi@codesyntax.com>
2012-09-06 17:09:33 +0200

    create canonical tag

--- cs/seopack/canonical.py
+++ cs/seopack/canonical.py
@@ -15,6 +15,15 @@ class CanonicalViewlet(ViewletBase):
         
         if self.pcs.canonical_object() is context:
             url = self.pcs.current_page_url()
+            position = url.find('?')
+            if position != -1:
+                # Remove the trailing / just before the parameters
+                if url[position-1] == '/':
+                    url = url[:position-1] + url[position:]
+            
+            if url.endswith('/'):
+                # Remove the trailing /
+                url = url[:-1]
         else:
             url = self.pcs.canonical_object_url()
             qs = self.request.get('QUERY_STRING', '')
