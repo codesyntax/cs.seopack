0f50aba5427696770eb8c0045647bfe1f2b1f680
Lur Ibargutxi <libargutxi@codesyntax.com>
2014-01-16 12:03:36 +0100

    espazioak

--- cs/seopack/noindex.py
+++ cs/seopack/noindex.py
@@ -10,13 +10,13 @@ class NoIndex(ViewletBase):
         if start is not None:
             return False
         context = aq_inner(self.context)
-        
+
         context_state = getMultiAdapter(
             (context, self.request), name=u'plone_context_state')
-        
+
         if context_state.is_default_page():
             current_page_url = context_state.current_page_url()
-        
+
             if context.absolute_url() == current_page_url:
                 return True
             else:
